b0e66a2ec1eeb0b3d8bc75b61781730aa197bee5
limtline <oww9037@naver.com>
2022-05-03 03:07:23 +0900

    전투 중 나가기 버튼 누르면 언제든 마을로 돌아가게 하는 버튼

---
+++ Unity/csExitBattle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class csExitBattle : MonoBehaviour
+{
+    public void ExitBattle()
+    {
+        Destroy(GameObject.FindObjectOfType<BattleManager>().gameObject);
+        GameObject.FindObjectOfType<csPlayerInfo>().townLoaded = false;
+        SceneManager.LoadScene("scPlay");
+    }
+}
