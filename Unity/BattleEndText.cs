88f353f5f6d5fc6d3a3eec060c7cbb6ecec7cd68
limtline <oww9037@naver.com>
2022-05-03 03:06:11 +0900

    전투 종료 시 나오는 멘트

---
+++ Unity/BattleEndText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleEndText : MonoBehaviour
+{
+    public GameObject success;
+    public GameObject fail;
+    public GameObject zero;
+    [HideInInspector]
+    public ArmyManager army;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        army = GameObject.FindObjectOfType<ArmyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("ShowText", 2);
+    }
+
+    void ShowText()
+    {
+        if (!GameObject.FindGameObjectWithTag("BUILDING"))
+            success.SetActive(true);
+
+        if (!GameObject.FindGameObjectWithTag("ATTACKUNIT")
+            && army.units.Find(u => u.name.Contains("soldier")).num == 0 && army.units.Find(u => u.name.Contains("archer")).num == 0
+            && army.units.Find(u => u.name.Contains("knight")).num == 0 && army.units.Find(u => u.name.Contains("horsearcher")).num == 0
+            && army.units.Find(u => u.name.Contains("cleric")).num == 0 && army.units.Find(u => u.name.Contains("wizard")).num == 0
+            && army.units.Find(u => u.name.Contains("catapult")).num == 0)
+            fail.SetActive(true);
+    }
+}
+
