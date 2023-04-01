043b5fa643469f9d31ae6a69662782fb665e9a36
limtline <oww9037@naver.com>
2023-04-01 20:33:55 +0900

    소리 조절

---
+++ Unity/VolumeCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeCtrl : MonoBehaviour
+{
+    public Slider sliBGM;
+    public Slider sliSFX;
+    public Toggle togMUTE;
+    
+    public void SetBgmVolume()
+    {
+        SoundManager.Instance.BgmVolume = sliBGM.value;
+        SoundManager.Instance.AudioSet();
+    }
+
+    public void SetSfxVolume()
+    {
+        SoundManager.Instance.SfxVolume = sliSFX.value;
+        SoundManager.Instance.AudioSet();
+    }
+
+    public void SetMute()
+    {
+        SoundManager.Instance.IsMute = togMUTE.isOn;
+        SoundManager.Instance.AudioSet();
+    }
+}
